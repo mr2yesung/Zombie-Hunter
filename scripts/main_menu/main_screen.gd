8cc2dfd5186ccf1190bde0a98928ce0956949976
mr2yesung <mryesung@naver.com>
2024-07-16 02:42:08 +0900

    create main screen with blue button theme, apply default font to the project

---
+++ scripts/main_menu/main_screen.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	# hide main screen, show weapon select screen
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	# hide main screen, show options screen
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
