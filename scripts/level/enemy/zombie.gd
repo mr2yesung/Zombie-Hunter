6a3f7dc90cae4c069376285c32292470de751a37
mr2yesung <mryesung@naver.com>
2024-07-18 20:32:19 +0900

    fix zombie look_at debug error when direction x and z are close to 0, occurs when player falls from 2nd floor or above

--- scripts/level/enemy/zombie.gd
+++ scripts/level/enemy/zombie.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 # AGGRO_RANGE = 30.0
 const AGGRO_RANGE_SQUARED := 900.0
 const ZOMBIE_RUN_ANIMATION_NAME := "zombie_animation_run"
+const GAME_OVER_DISTANCE_SQUARED := 7.0
+const MINIMUM_LOOKAT_DISTANCE_SQUARED := 0.0001
 
 @export var walk_speed := 5.0
 @export var run_speed := 10.0
@@ -31,7 +33,7 @@ func _physics_process(delta: float) -> void:
 	var direction := global_position.direction_to(next_position)
 	var distance := global_position.distance_squared_to(player.global_position)
 	
-	if distance <= 5.0:
+	if distance <= GAME_OVER_DISTANCE_SQUARED:
 		# TODO: add game over
 		print("player died")
 	
@@ -41,9 +43,12 @@ func _physics_process(delta: float) -> void:
 	
 	var speed := run_speed if provoked else walk_speed
 	
-	if direction != Vector3.ZERO:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
-		look_at(Vector3(direction.x, 0, direction.z) + global_position, Vector3.UP, true)
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	
+	var look_at_direction := Vector3(direction.x, 0.0, direction.z)
+	
+	if look_at_direction.length_squared() > MINIMUM_LOOKAT_DISTANCE_SQUARED:
+		look_at(look_at_direction + global_position, Vector3.UP, true)
 	
 	move_and_slide()
